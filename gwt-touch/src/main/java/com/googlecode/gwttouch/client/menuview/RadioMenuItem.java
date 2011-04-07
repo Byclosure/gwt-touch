dfec7a05c593c7309533789a60a7043a4f7f857b
brad.rydzewski <brad.rydzewski@94422487-5008-8ea5-abca-8e9d5f75ee8b>
2011-04-07 06:54:24 +0000

    Updated RadioMenuItem to implement HasValue<Boolean>

--- gwt-touch/src/main/java/com/googlecode/gwttouch/client/menuview/RadioMenuItem.java
+++ gwt-touch/src/main/java/com/googlecode/gwttouch/client/menuview/RadioMenuItem.java
@@ -1,14 +1,17 @@
 package com.googlecode.gwttouch.client.menuview;
 
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasName;
 import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RadioButton;
 
-public class RadioMenuItem extends Composite implements HasText, HasName {
+public class RadioMenuItem extends Composite implements HasText, HasName, HasValue<Boolean> {
 
 	private FlowPanel root = new FlowPanel();
 	private final Label label = new Label();
@@ -45,6 +48,27 @@ public class RadioMenuItem extends Composite implements HasText, HasName {
 	@Override
 	public String getName() {
 		return radio.getName();
+	}
+
+	@Override
+	public HandlerRegistration addValueChangeHandler(
+			ValueChangeHandler<Boolean> handler) {
+		return radio.addValueChangeHandler(handler);
+	}
+
+	@Override
+	public Boolean getValue() {
+		return radio.getValue();
+	}
+
+	@Override
+	public void setValue(Boolean value) {
+		radio.setValue(value);
+	}
+
+	@Override
+	public void setValue(Boolean value, boolean fireEvents) {
+		radio.setValue(value, fireEvents);
 	}
 
 }
